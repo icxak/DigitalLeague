28ca4dea75226dd3464d497952561506a13a1deb
icxak <iskhak_26@mail.ru>
2021-09-23 14:07:06 +0300

    test

--- 1_Calculator/src/main/java/Main.java
+++ 1_Calculator/src/main/java/Main.java
@@ -2,12 +2,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter your Expression: ");
-        Scanner sc = new Scanner(System.in);
-        String expression = sc.nextLine();
-        String expressionWithoutSpaces = expression.replaceAll("\\s+", "");
 
 
-        System.out.println(Calculator.RPNtoAnswer(Calculator.ExpressionToRpn(expressionWithoutSpaces)));
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+
+        }
+        int num =2 ;
+
+        String intToString = Integer.toString(num);
+        System.out.println(intToString);
+        int ss=intToString.charAt(0);
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//        System.out.println("Enter your Expression: ");
+//        Scanner sc = new Scanner(System.in);
+//
+//        String expression = sc.nextLine();
+//
+//        String expressionWithoutSpaces = expression.replaceAll("\\s+", "");
+//
+//
+//        System.out.println(Calculator.RPNtoAnswer(Calculator.ExpressionToRpn(expressionWithoutSpaces)));
     }
+    public static Double multiply(Double a, Double b) {
+        return a * b;}
 }
